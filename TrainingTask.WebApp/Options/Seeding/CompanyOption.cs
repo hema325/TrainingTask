4b8babb1f86856cd432da0c4491550dc4dafb997
hema325 <hema24790@gmail.com>
2023-07-02 11:09:44 +0300

    changeSeedingFolderName

---
+++ TrainingTask.WebApp/Options/Seeding/CompanyOption.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TrainingTask.WebApp.Common.Mapping;
+using TrainingTask.WebApp.Entities;
+
+namespace TrainingTask.WebApp.Options.Seeding
+{
+    public class CompanyOption : IMapTo<Company>
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+        public string? Notes { get; set; }
+    }
+}
---
+++ TrainingTask.WebApp/Options/Seeding/SeedingOption.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TrainingTask.WebApp.Options.Seeding
+{
+    public class SeedingOption
+    {
+        public const string Seeding = "Seeding";
+        public IEnumerable<ClientOption> Clients { get; set; }
+        public IEnumerable<CompanyOption> Companies { get; set; }
+        public IEnumerable<InvoiceOption> Invoices { get; set; }
+        public IEnumerable<ItemOption> Items { get; set; }
+        public IEnumerable<TypeOption> Types { get; set; }
+        public IEnumerable<UnitOption> Units { get; set; }
+    }
+}
